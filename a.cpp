5bd6672bcc809c45863130e8c49f5fe30a8a4637
Igor Buzin <buzin1820@gmail.com>
2026-01-28 07:05:20 +0000

    check precommit

---
+++ a.cpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstdint>
+#include <istream>
+#include <vector>
+
+class CSVReader {
+public:
+    CSVReader(std::istream *input, char delim = ',');
+    std::vector<std::string> GetRow(uint8_t cnt_columns);
+    bool IsRead();
+
+private:
+    std::istream *input_{nullptr};
+  char delim_;
+};
